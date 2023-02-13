6fec1a751cf1d46d492b71b5f4c968c48bc8053d
philypy-cg <phcg.dev@gmail.com>
2023-02-13 00:13:27 -0300

    primeiro programa java

---
+++ primeiro_programa_java/src/primeiro_programa_java/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package primeiro_programa_java;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2;
+		int b = 3;
+		System.out.println("Hello World!" + (a+b));
+	}
+
+}
---
+++ primeiro_programa_java/src/primeiro_programa_java_model/Gato.java
@@ -0,0 +1,14 @@
+package primeiro_programa_java_model;
+
+public class Gato {
+	private String nome;
+	private String cor;
+	private String idade;
+	
+	public Gato (String nome, String Cor, Integer Idade){
+		this.nome= nome;
+		this.cor= cor;
+		this.idade= idade;
+	}
+
+}
